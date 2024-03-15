890d5fd0f8cec0246d4efd8aaa510a23fb82d39d
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-16 01:36:07 +0530

    Importing necessary libraries

--- text-check.py
+++ text-check.py
@@ -1,1 +1,8 @@
-
+#Importing necessary libraries
+import re
+import string
+from collections import Counter
+from wordcloud import WordCloud
+from nltk.tokenize import sent_tokenize, word_tokenize
+import matplotlib.pyplot as plt
+from nltk.util import ngrams
