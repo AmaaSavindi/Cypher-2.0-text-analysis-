e23e2579e622763bd19e7a42beb810f404d3dfa0
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-16 01:40:52 +0530

    Function to clean the text and generate identifier

--- text-check.py
+++ text-check.py
@@ -6,3 +6,12 @@ from wordcloud import WordCloud
 from nltk.tokenize import sent_tokenize, word_tokenize
 import matplotlib.pyplot as plt
 from nltk.util import ngrams
+
+# Function to clean the text and generate identifier
+def clean_identified_text(text):
+    # Remove punctuation
+    # Convert text to lowercase
+    text = text.translate(str.maketrans('', '', string.punctuation)).lower()
+    # Generating identifier to the text
+    identifier = word_tokenize(text)
+    return identifier
